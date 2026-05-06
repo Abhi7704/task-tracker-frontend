f5c381aa789fa4b8b63bbc614ac4e4bc7fac7c3b
Abhinav Vinod <abhinavvinod1067@gmail.com>
2026-05-06 15:48:10 +0400

    Add error logging

--- Backend/server.js
+++ Backend/server.js
@@ -3,6 +3,15 @@ const mongoose = require('mongoose');
 const cors     = require('cors');
 require('dotenv').config();
 
+process.on('uncaughtException', (err) => {
+  console.error('Uncaught Exception:', err);
+  process.exit(1);
+});
+process.on('unhandledRejection', (reason) => {
+  console.error('Unhandled Rejection:', reason);
+  process.exit(1);
+});
+
 const app = express();
 
 app.use(cors({ origin: '*' }));
